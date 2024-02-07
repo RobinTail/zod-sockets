b92cf47f1fc443a2eddeafb774a956def9e70813
Robin Tail <robin_tail@me.com>
2024-02-07 12:46:53 +0100

    Fix tests.

--- src/actions-factory.spec.ts
+++ src/actions-factory.spec.ts
@@ -2,9 +2,14 @@ import { describe, expect, test, vi } from "vitest";
 import { z } from "zod";
 import { Action } from "./action";
 import { ActionsFactory } from "./actions-factory";
+import { AbstractLogger } from "./logger";
 
 describe("ActionsFactory", () => {
-  const factory = new ActionsFactory({ emission: {}, timeout: 2000 });
+  const factory = new ActionsFactory({
+    emission: {},
+    timeout: 2000,
+    logger: { debug: vi.fn() } as unknown as AbstractLogger,
+  });
 
   describe("constructor", () => {
     test("should create a factory", () => {

134d7320950743269fb0f92aa2a758a2706e46b5
Robin Tail <robin_tail@me.com>
2024-02-07 12:52:49 +0100

    Declaring example actions inline.

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,9 @@
 import http from "node:http";
 import { attachSockets } from "../src";
+import { onChat } from "./actions/chat";
+import { onPing } from "./actions/ping";
+import { onSubscribe } from "./actions/subscribe";
 import { config } from "./config";
-import { actions } from "./routing";
 import { Server } from "socket.io";
 
 const target = new http.Server().listen(8090);
@@ -10,5 +12,10 @@ attachSockets({
   io: new Server(),
   config,
   target,
-  actions,
+  actions: {
+    /** @desc the object declares handling rules of the incoming socket.io events */
+    ping: onPing,
+    subscribe: onSubscribe,
+    chat: onChat,
+  },
 });

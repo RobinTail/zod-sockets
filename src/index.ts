2095357bfdf579aa0d7b4904d3f981c3d413b693
Robin Tail <robin_tail@me.com>
2024-02-08 07:02:12 +0100

    Issue 952: exposing HandlingFeatures.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { attachSockets } from "./attach";
 export { createConfig, type SocketsConfig } from "./config";
 export { ActionsFactory } from "./actions-factory";
-export type { ActionMap, SocketFeatures } from "./action";
+export type { ActionMap, SocketFeatures, HandlingFeatures } from "./action";
 
 // issue 952
 export type { EmissionMap } from "./emission";

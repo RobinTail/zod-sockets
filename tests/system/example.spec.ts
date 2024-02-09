d3f7455cdcc7a0017a2cda2e295aca509ecb77a7
Robin Tail <robin_tail@me.com>
2024-02-09 21:22:27 +0100

    Subscribe event test.

--- tests/system/example.spec.ts
+++ tests/system/example.spec.ts
@@ -1,6 +1,7 @@
 import { spawn } from "node:child_process";
 import { afterAll, describe, expect, test, vi } from "vitest";
 import { io } from "socket.io-client";
+import { z } from "zod";
 import { waitFor } from "../helpers";
 
 describe("System test on Example", async () => {
@@ -30,6 +31,21 @@ describe("System test on Example", async () => {
       });
       client.emit("ping", "test", ack);
       await waitFor(() => ack.mock.calls.length === 1);
+    });
+  });
+
+  describe("subscribe", () => {
+    test("should receive time events every second", async () => {
+      const onTime = vi.fn((...response) => {
+        expect(response).toEqual([expect.any(String)]);
+        expect(
+          z.string().datetime().safeParse(response[0]).success,
+        ).toBeTruthy();
+      });
+      client.on("time", onTime);
+      client.emit("subscribe");
+      await waitFor(() => onTime.mock.calls.length > 1);
+      client.off("time", onTime);
     });
   });
 });

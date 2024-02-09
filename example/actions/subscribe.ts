dde6e10aeb79aaaf587d374175e4b66bc36845f6
Robin Tail <robin_tail@me.com>
2024-02-09 10:30:03 +0100

    Client and All scopes.

--- example/actions/subscribe.ts
+++ example/actions/subscribe.ts
@@ -4,11 +4,11 @@ import { actionsFactory } from "../factories";
 /** @desc The action demonstrates no acknowledgement and constraints on emission awareness */
 export const onSubscribe = actionsFactory.build({
   input: z.tuple([]).rest(z.unknown()),
-  handler: async ({ logger, emit, client }) => {
+  handler: async ({ logger, client }) => {
     logger.info("Subscribed");
     while (true) {
       try {
-        emit("time", new Date()); // <— payload type constraints
+        await client.emit("time", new Date()); // <— payload type constraints
         await new Promise<void>((resolve, reject) => {
           const timer = setTimeout(() => {
             clearTimeout(timer);

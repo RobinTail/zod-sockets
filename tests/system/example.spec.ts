7fd5b94e9c1e119a422961d03034ea5f3ce16276
Robin Tail <robin_tail@me.com>
2024-02-09 21:14:32 +0100

    System: ping test.

---
+++ tests/system/example.spec.ts
@@ -0,0 +1,35 @@
+import { spawn } from "node:child_process";
+import { afterAll, describe, expect, test, vi } from "vitest";
+import { io } from "socket.io-client";
+import { waitFor } from "../helpers";
+
+describe("System test on Example", async () => {
+  let out = "";
+  const listener = (chunk: Buffer) => {
+    out += chunk.toString();
+  };
+
+  const example = spawn("tsx", ["index.ts"], {
+    cwd: "./example",
+  });
+  example.stdout.on("data", listener);
+  await waitFor(() => out.indexOf("ZOD-SOCKETS") > -1);
+  const client = io("ws://localhost:8090");
+  await waitFor(() => client.connected);
+
+  afterAll(async () => {
+    example.stdout.removeListener("data", listener);
+    example.kill();
+    await waitFor(() => example.killed);
+  });
+
+  describe("ping", () => {
+    test("should ack. pong with echo", async () => {
+      const ack = vi.fn((...response) => {
+        expect(response).toEqual(["pong", "test"]);
+      });
+      client.emit("ping", "test", ack);
+      await waitFor(() => ack.mock.calls.length === 1);
+    });
+  });
+});

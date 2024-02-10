4656fc4e2660e0eaaf7dd2a7f68fa7fdc64728e0
Robin Tail <robin_tail@me.com>
2024-02-10 14:37:49 +0100

    Add join to RemoteClient.

--- src/remote-client.ts
+++ src/remote-client.ts
@@ -4,11 +4,13 @@ export interface RemoteClient {
   id: string;
   rooms: string[];
   getData: <D extends object>() => Readonly<Partial<D>>;
+  join: (rooms: string | string[]) => void | Promise<void>;
 }
 
 export const getRemoteClients = (sockets: RemoteSocket<{}, unknown>[]) =>
-  sockets.map<RemoteClient>(({ id, rooms, data }) => ({
+  sockets.map<RemoteClient>(({ id, rooms, data, join }) => ({
     id: id,
     rooms: Array.from(rooms),
     getData: <D extends object>() => (data as D) || {},
+    join,
   }));

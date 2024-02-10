9f83689dc290fd753b7bb3153740b053b48c768d
Anna Bocharova <robin_tail@me.com>
2024-02-10 11:56:54 +0100

    Contexts (#23)
    * Moving Client to a separate file.
    
    * Moving Handler and HandlingFeatures to a separate file.
    
    * Moving join/leave to client. Replacing HandlingFeatures with 3 inheriting contexts.
    
    * Issue 952: exposing Client and RemoteClient.
    
    * Ref: more generic debug msg in makeGenericEmitter.
    
    * Ref: CtxNoInput is actually ClientContext.
    
    * Exposing ClientContext.
    
    * Rev: unexposing Client and RemoteClient.

--- src/emission.ts
+++ src/emission.ts
@@ -30,13 +30,11 @@ export type Broadcaster<E extends EmissionMap> = <K extends keyof E>(
 
 export type RoomService<E extends EmissionMap> = (rooms: string | string[]) => {
   /**
-   * @desc Emits an event to everyone in the specified room(s)
+   * @desc Emits an event to others in the specified room(s)
    * @throws z.ZodError on validation
    * @throws Error on ack timeout
    * */
   broadcast: Broadcaster<E>;
-  join: () => void | Promise<void>;
-  leave: () => void | Promise<void>;
   getClients: () => Promise<RemoteClient[]>;
 };
 
@@ -57,10 +55,7 @@ const makeGenericEmitter =
     assert(event in emission, new Error(`Unsupported event ${event}`));
     const { schema, ack } = emission[event];
     const payload = schema.parse(args);
-    logger.debug(
-      `${isSocket ? "Emitting" : "Broadcasting"} ${String(event)}`,
-      payload,
-    );
+    logger.debug(`Sending ${String(event)}`, payload);
     if (!ack) {
       return target.emit(String(event), ...payload) || true;
     }
@@ -93,11 +88,6 @@ export const makeRoomService =
   (rooms) => ({
     getClients: async () =>
       getRemoteClients(await socket.in(rooms).fetchSockets()),
-    join: () => socket.join(rooms),
-    leave: () =>
-      typeof rooms === "string"
-        ? socket.leave(rooms)
-        : Promise.all(rooms.map((room) => socket.leave(room))).then(() => {}),
     broadcast: makeGenericEmitter({
       ...rest,
       target: socket.to(rooms),
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export { attachSockets } from "./attach";
 export { createConfig, type Config } from "./config";
 export { ActionsFactory } from "./actions-factory";
-export type { ActionMap, HandlingFeatures } from "./action";
+export type { ActionMap } from "./action";
 
 // issue 952
 export type { EmissionMap } from "./emission";
 export type { LoggerOverrides } from "./logger";
+export type { ClientContext } from "./handler";

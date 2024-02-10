ab5f856ed6a97b92bbbc7fd93f196cf57bd75f9f
Anna Bocharova <robin_tail@me.com>
2024-02-10 15:39:51 +0100

    Generic distribution methods (#28)
    * Simpler joiner impl.
    
    * Add join to RemoteClient.
    
    * makeDistribution(), join and leave methods for remote client.
    
    * Prevent: binding join method to the subject.
    
    * Changelog: 0.6.1.

--- src/remote-client.ts
+++ src/remote-client.ts
@@ -1,14 +1,17 @@
 import { RemoteSocket } from "socket.io";
 
-export interface RemoteClient {
+import { Distribution, makeDistribution } from "./distribution";
+
+export interface RemoteClient extends Distribution {
   id: string;
   rooms: string[];
   getData: <D extends object>() => Readonly<Partial<D>>;
 }
 
 export const getRemoteClients = (sockets: RemoteSocket<{}, unknown>[]) =>
-  sockets.map<RemoteClient>(({ id, rooms, data }) => ({
-    id: id,
-    rooms: Array.from(rooms),
-    getData: <D extends object>() => (data as D) || {},
+  sockets.map<RemoteClient>((socket) => ({
+    id: socket.id,
+    rooms: Array.from(socket.rooms),
+    getData: <D extends object>() => (socket.data as D) || {},
+    ...makeDistribution(socket),
   }));

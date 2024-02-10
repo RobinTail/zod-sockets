5bc3f820f0075b24675cde71aad0dbc5c3551ab2
Robin Tail <robin_tail@me.com>
2024-02-10 11:54:59 +0100

    Rev: unexposing Client and RemoteClient.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,4 @@ export type { ActionMap } from "./action";
 // issue 952
 export type { EmissionMap } from "./emission";
 export type { LoggerOverrides } from "./logger";
-export type { Client } from "./client";
-export type { RemoteClient } from "./remote-client";
 export type { ClientContext } from "./handler";

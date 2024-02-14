4cb9213e92844ce74d3f2eb0707f9ab0798feb65
Robin Tail <robin_tail@me.com>
2024-02-15 00:07:11 +0100

    Action definition to accept the optional namespace.

--- src/actions-factory.ts
+++ src/actions-factory.ts
@@ -3,35 +3,44 @@ import { Action } from "./action";
 import { Config } from "./config";
 import { EmissionMap } from "./emission";
 import { ActionContext, Handler } from "./handler";
+import { SomeNamespaces } from "./namespace";
 
 export interface ActionNoAckDef<
   IN extends z.AnyZodTuple,
-  E extends EmissionMap,
+  NS extends SomeNamespaces<EmissionMap>,
+  K extends keyof NS,
 > {
+  ns?: K;
   /** @desc The incoming event payload validation schema (no acknowledgement) */
   input: IN;
   /** @desc No output schema => no returns => no acknowledgement */
-  handler: Handler<ActionContext<z.output<IN>, E>, void>;
+  handler: Handler<ActionContext<z.output<IN>, NS[K]>, void>;
 }
 
 export interface ActionWithAckDef<
   IN extends z.AnyZodTuple,
   OUT extends z.AnyZodTuple,
-  E extends EmissionMap,
+  NS extends SomeNamespaces<EmissionMap>,
+  K extends keyof NS,
 > {
+  ns?: K;
   /** @desc The incoming event payload (excl. acknowledgement) validation schema */
   input: IN;
   /** @desc The acknowledgement validation schema */
   output: OUT;
   /** @desc The returns become an Acknowledgement */
-  handler: Handler<ActionContext<z.output<IN>, E>, z.input<OUT>>;
+  handler: Handler<ActionContext<z.output<IN>, NS[K]>, z.input<OUT>>;
 }
 
-export class ActionsFactory<E extends EmissionMap> {
-  constructor(protected config: Config<E>) {}
+export class ActionsFactory<NS extends SomeNamespaces<EmissionMap>> {
+  constructor(protected config: Config<NS>) {}
 
-  public build<IN extends z.AnyZodTuple, OUT extends z.AnyZodTuple>(
-    def: ActionNoAckDef<IN, E> | ActionWithAckDef<IN, OUT, E>,
+  public build<
+    IN extends z.AnyZodTuple,
+    OUT extends z.AnyZodTuple,
+    K extends keyof NS = "/",
+  >(
+    def: ActionNoAckDef<IN, NS, K> | ActionWithAckDef<IN, OUT, NS, K>,
   ): Action<IN, OUT> {
     return new Action(def);
   }

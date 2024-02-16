5cf24c61f23c6faaa328c5d707a4046b3be6c986
Robin Tail <robin_tail@me.com>
2024-02-16 10:34:15 +0100

    FIX: duplicated hooks definition.

--- src/attach.ts
+++ src/attach.ts
@@ -10,29 +10,11 @@ import {
   makeEmitter,
   makeRoomService,
 } from "./emission";
-import {
-  ClientContext,
-  Handler,
-  IndependentContext,
-  TracingContext,
-} from "./handler";
+import { ClientContext, IndependentContext } from "./handler";
+import { HookSet, Hooks } from "./hooks";
 import { Namespaces, ensureNamespaces, normalizeNS } from "./namespaces";
 import { getRemoteClients } from "./remote-client";
 import { getStartupLogo } from "./startup-logo";
-
-interface HookSet<E extends EmissionMap> {
-  /** @desc A place for emitting events regardless receiving events */
-  onConnection?: Handler<ClientContext<E>, void>;
-  onDisconnect?: Handler<ClientContext<E>, void>;
-  onAnyIncoming?: Handler<TracingContext<E>, void>;
-  onAnyOutgoing?: Handler<TracingContext<E>, void>;
-  /** @desc A place for emitting events regardless clients activity */
-  onStartup?: Handler<IndependentContext<E>, void>;
-}
-
-type Hooks<NS extends Namespaces<EmissionMap>> = {
-  [K in keyof NS]?: HookSet<NS[K]>;
-};
 
 export const attachSockets = async <NS extends Namespaces<EmissionMap>>({
   io,

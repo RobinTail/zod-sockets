8d33c628d808b07c02f0aed4d33214e5a1ab580f
Robin Tail <robin_tail@me.com>
2024-02-25 15:00:08 +0100

    Ref: DNRY: extracting actions.

--- example/generate-client.ts
+++ example/generate-client.ts
@@ -1,12 +1,10 @@
 import { writeFile } from "node:fs/promises";
 import { Integration } from "../src/integration";
-import { onChat } from "./actions/chat";
-import { onPing } from "./actions/ping";
-import { onSubscribe } from "./actions/subscribe";
+import { actions } from "./actions";
 import { config } from "./config";
 
 await writeFile(
   "example/example-client.ts",
-  new Integration({ config, actions: [onChat, onPing, onSubscribe] }).print(),
+  new Integration({ config, actions }).print(),
   "utf-8",
 );
--- example/index.ts
+++ example/index.ts
@@ -1,16 +1,14 @@
 import http from "node:http";
 import { attachSockets } from "../src";
-import { onChat } from "./actions/chat";
-import { onPing } from "./actions/ping";
-import { onSubscribe } from "./actions/subscribe";
+import { actions } from "./actions";
 import { config } from "./config";
 import { Server } from "socket.io";
 
 attachSockets({
   io: new Server(),
-  config,
   target: http.createServer().listen(8090),
-  actions: [onPing, onSubscribe, onChat],
+  config,
+  actions,
   hooks: {
     onConnection: async ({ client }) => {
       await client.broadcast("chat", `${client.id} entered the chat`, {

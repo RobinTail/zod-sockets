73006d93f2a1a8a6743d2f73c775733fd0cee6ba
Robin Tail <robin_tail@me.com>
2024-02-25 21:03:18 +0100

    Exposing required entities.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { attachSockets } from "./attach";
 export { createConfig, type Config } from "./config";
 export { ActionsFactory } from "./actions-factory";
+export { AbstractAction } from "./action";
+export { Integration } from "./integration";
 
 // issue 952
 export type { EmissionMap } from "./emission";

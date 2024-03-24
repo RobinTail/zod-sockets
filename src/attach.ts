4253e05ea0fdf67a66a7b4134126ed0d6fb16b33
Robin Tail <robin_tail@me.com>
2024-03-24 22:19:45 +0100

    Using chalk in runtime.

--- src/attach.ts
+++ src/attach.ts
@@ -119,7 +119,9 @@ export const attachSockets = async <NS extends Namespaces<EmissionMap>>({
     });
     await onStartup(nsCtx);
   }
-  (startupLogo ? console.log : () => {})(getStartupLogo());
+  (startupLogo ? console.log : () => {})(
+    getStartupLogo((await import("chalk")).default), // chalk v5 is ESM only
+  );
   rootLogger.info("Listening", target.address());
   return io.attach(target);
 };

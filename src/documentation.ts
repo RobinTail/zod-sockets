f8e41b36dd4bf366f173689e560a1daa9c876fb2
Robin Tail <robin_tail@me.com>
2024-03-26 21:58:16 +0100

    Fix: making channel bindings once.

--- src/documentation.ts
+++ src/documentation.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { AbstractAction } from "./action";
 import { AsyncApiDocumentBuilder } from "./async-api/document-builder";
 import { AsyncChannelObject } from "./async-api/commons";
+import { SocketIOChannelBinding } from "./async-api/socket-io-binding";
 import { Config } from "./config";
 import { depicters, onEach, onMissing } from "./documentation-helpers";
 import { Namespaces, normalizeNS } from "./namespace";
@@ -44,46 +45,45 @@ export class Documentation extends AsyncApiDocumentBuilder {
         this.document.id = `urn:${uri.host.split(".").concat(uri.pathname.slice(1).split("/")).join(":")}`;
       }
     }
+    const channelBinding: SocketIOChannelBinding = {
+      bindingVersion: "0.11.0",
+      method: "GET",
+      headers: walkSchema({
+        direction: "in",
+        schema: z.object({
+          connection: z.literal("Upgrade").optional(),
+          upgrade: z.literal("websocket").optional(),
+        }),
+        onEach,
+        onMissing,
+        rules: depicters,
+      }),
+      query: {
+        ...walkSchema({
+          direction: "in",
+          schema: z.object({
+            EIO: z
+              .literal("4")
+              .describe("Mandatory, the version of the protocol"),
+            transport: z
+              .union([z.literal("polling"), z.literal("websocket")])
+              .describe("Mandatory, the name of the transport."),
+            sid: z.string().optional().describe("The session identifier"),
+          }),
+          onEach,
+          onMissing,
+          rules: depicters,
+        }),
+        externalDocs: {
+          description: "Engine.IO Protocol",
+          url: "https://socket.io/docs/v4/engine-io-protocol/",
+        },
+      },
+    };
     for (const [ns, { emission }] of Object.entries(namespaces)) {
       const channel: AsyncChannelObject = {
         description: `${normalizeNS(ns)} namespace`,
-        bindings: {
-          "socket.io": {
-            bindingVersion: "0.11.0",
-            method: "GET",
-            headers: walkSchema({
-              direction: "in",
-              schema: z.object({
-                connection: z.literal("Upgrade").optional(),
-                upgrade: z.literal("websocket").optional(),
-              }),
-              onEach,
-              onMissing,
-              rules: depicters,
-            }),
-            query: {
-              ...walkSchema({
-                direction: "in",
-                schema: z.object({
-                  EIO: z
-                    .literal("4")
-                    .describe("Mandatory, the version of the protocol"),
-                  transport: z
-                    .union([z.literal("polling"), z.literal("websocket")])
-                    .describe("Mandatory, the name of the transport."),
-                  sid: z.string().optional().describe("The session identifier"),
-                }),
-                onEach,
-                onMissing,
-                rules: depicters,
-              }),
-              externalDocs: {
-                description: "Engine.IO Protocol",
-                url: "https://socket.io/docs/v4/engine-io-protocol/",
-              },
-            },
-          },
-        },
+        bindings: { "socket.io": channelBinding },
         subscribe: {
           operationId: `out${normalizeNS(ns)}`,
           description: `The messages produced by the application within the ${normalizeNS(ns)} namespace`,

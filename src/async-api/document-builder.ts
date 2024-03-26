14735fe741c0a20a4a48d912e03d1b9eadbe654a
Robin Tail <robin_tail@me.com>
2024-03-26 20:11:11 +0100

    Removing redundant methods from builder.

--- src/async-api/document-builder.ts
+++ src/async-api/document-builder.ts
@@ -1,13 +1,6 @@
-import type {
-  ExternalDocumentationObject,
-  SecuritySchemeObject,
-  TagObject,
-} from "openapi3-ts/oas31";
-import { complement, isNil, pickBy } from "ramda";
 import {
   AsyncApiDocument,
   AsyncChannelObject,
-  AsyncSecuritySchemeObject,
   AsyncServerObject,
 } from "./commons";
 import yaml from "yaml";
@@ -33,125 +26,6 @@ export class AsyncApiDocumentBuilder {
     return this;
   }
 
-  public addServers(
-    servers: { name: string; server: AsyncServerObject }[],
-  ): this {
-    for (const { name, server } of servers) {
-      this.addServer(name, server);
-    }
-
-    return this;
-  }
-
-  public setExternalDoc(description: string, url: string): this {
-    this.document.externalDocs = { description, url };
-    return this;
-  }
-
-  public setDefaultContentType(contentType: string) {
-    this.document.defaultContentType = contentType;
-    return this;
-  }
-
-  public addTag(
-    name: string,
-    description?: string,
-    externalDocs?: ExternalDocumentationObject,
-  ): this {
-    this.document.tags = [...(this.document.tags || [])].concat(
-      pickBy(complement(isNil), {
-        name,
-        description,
-        externalDocs,
-      }) satisfies TagObject,
-    );
-    return this;
-  }
-
-  public addSecurity(name: string, options: AsyncSecuritySchemeObject): this {
-    this.document.components = {
-      ...this.document.components,
-      securitySchemes: {
-        ...this.document.components?.securitySchemes,
-        [name]: options,
-      },
-    };
-    return this;
-  }
-
-  public addBearerAuth(
-    options: SecuritySchemeObject = {
-      type: "http",
-    },
-    name = "bearer",
-  ): this {
-    this.addSecurity(name, {
-      scheme: "bearer",
-      bearerFormat: "JWT",
-      ...options,
-    });
-    return this;
-  }
-
-  public addOAuth2(
-    options: SecuritySchemeObject = {
-      type: "oauth2",
-    },
-    name = "oauth2",
-  ): this {
-    this.addSecurity(name, {
-      flows: {},
-      ...options,
-      type: "oauth2",
-    });
-    return this;
-  }
-
-  public addApiKey(
-    options: SecuritySchemeObject = {
-      type: "apiKey",
-    },
-    name = "api_key",
-  ): this {
-    this.addSecurity(name, {
-      in: "header",
-      name,
-      ...options,
-      type: "apiKey",
-    });
-    return this;
-  }
-
-  public addBasicAuth(
-    options: SecuritySchemeObject = {
-      type: "http",
-    },
-    name = "basic",
-  ): this {
-    this.addSecurity(name, {
-      scheme: "basic",
-      ...options,
-      type: "http",
-    });
-    return this;
-  }
-
-  public addCookieAuth(
-    cookieName = "connect.sid",
-    options: SecuritySchemeObject = {
-      type: "apiKey",
-    },
-    securityName = "cookie",
-  ): this {
-    this.addSecurity(securityName, {
-      in: "cookie",
-      name: cookieName,
-      ...options,
-      type: "apiKey",
-    });
-    return this;
-  }
-
   public addChannel(name: string, channel: AsyncChannelObject): this {
     this.document.channels = { ...this.document.channels, [name]: channel };
     return this;
@@ -167,6 +41,7 @@ export class AsyncApiDocumentBuilder {
   ): string {
     return JSON.stringify(this.document, replacer, space);
   }
+
   getSpecAsYaml(): string {
     return yaml.stringify(this.document);
   }

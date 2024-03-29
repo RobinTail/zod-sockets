486f6f59fdb070be6decca1e8ac34e12d67cee73
Robin Tail <robin_tail@me.com>
2024-03-29 15:00:20 +0100

    SchemaObject described.

--- src/async-api/commons.ts
+++ src/async-api/commons.ts
@@ -1,4 +1,3 @@
-import type { SchemaObject } from "openapi3-ts/oas31";
 import {
   WSChannelBinding,
   WSMessageBinding,
@@ -97,6 +96,91 @@ export interface ServerVariableObject {
   default: string | boolean | number;
   description?: string;
   examples?: string[];
+}
+
+export type SchemaObjectType =
+  | "integer"
+  | "number"
+  | "string"
+  | "boolean"
+  | "object"
+  | "null"
+  | "array";
+
+/**
+ * @desc extends DRAFT-07
+ * @link https://json-schema.org/specification-links#draft-7
+ * @link https://json-schema.org/draft-07/draft-handrews-json-schema-validation-01
+ * */
+interface Draft07 {
+  title?: string;
+  type?: SchemaObjectType | SchemaObjectType[];
+  required?: string[];
+  multipleOf?: number;
+  maximum?: number;
+  exclusiveMaximum?: number;
+  minimum?: number;
+  exclusiveMinimum?: number;
+  maxLength?: number;
+  minLength?: number;
+  pattern?: string;
+  maxItems?: number;
+  minItems?: number;
+  uniqueItems?: boolean; // @todo explore
+  maxProperties?: number;
+  minProperties?: number;
+  enum?: any[];
+  const: any; // @todo explore: enum of single value
+  examples?: any[];
+  // if,then,else
+  readOnly?: boolean;
+  writeOnly?: boolean;
+  properties?: { [propertyName: string]: SchemaObject | ReferenceObject };
+  patternProperties?: { [pattern: string]: SchemaObject | ReferenceObject }; // @todo explore
+  additionalProperties?: SchemaObject | ReferenceObject | boolean;
+  additionalItems?: SchemaObject;
+  items?: SchemaObject | ReferenceObject;
+  propertyNames?: SchemaObject | ReferenceObject; // @todo explore
+  contains?: SchemaObject; // @todo explore
+  allOf?: (SchemaObject | ReferenceObject)[];
+  oneOf?: (SchemaObject | ReferenceObject)[];
+  anyOf?: (SchemaObject | ReferenceObject)[];
+  not?: SchemaObject | ReferenceObject;
+  /**
+   * @desc JSON Schema compliant Content-Type, optional when specified as a key of ContentObject
+   * @example image/png
+   */
+  contentMediaType?: string;
+  /**
+   * @desc Specifies the Content-Encoding for the schema, supports all encodings from RFC4648, and "quoted-printable" from RFC2045
+   * @override format
+   * @see https://datatracker.ietf.org/doc/html/rfc4648
+   * @see https://datatracker.ietf.org/doc/html/rfc2045#section-6.7
+   * @example base64
+   */
+  contentEncoding?: string;
+}
+
+/** @link https://www.asyncapi.com/docs/reference/specification/v3.0.0#schemaObject */
+export interface SchemaObject extends Draft07 {
+  // overrides:
+  description?: string; // supports markdown
+  format?:
+    | "int32" // no changes, but declared as more loose
+    | "int64"
+    | "float"
+    | "double"
+    | "byte"
+    | "binary"
+    | "date"
+    | "date-time"
+    | "password"
+    | string;
+  default?: any; // must comply the type
+  // proprietary:
+  discriminator?: string;
+  externalDocs?: ExternalDocumentationObject | ReferenceObject;
+  deprecated?: boolean;
 }
 
 /** @since 3.0.0 added replies */

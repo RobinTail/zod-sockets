143ebee60131a3bab96db355094b8895186fbdf9
Robin Tail <robin_tail@me.com>
2024-03-30 20:41:50 +0100

    Testing AsyncAPI helpers.

---
+++ src/async-api/helpers.spec.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest";
+import { isReferenceObject } from "./helpers";
+
+describe("AsyncAPI Helpers", () => {
+  describe("isReferenceObject", () => {
+    test("should detect the reference object by the presence of the $ref prop", () => {
+      expect(isReferenceObject({})).toBeFalsy();
+      expect(isReferenceObject({ type: "null" })).toBeFalsy();
+      expect(isReferenceObject({ $ref: "" })).toBeTruthy();
+    });
+  });
+});

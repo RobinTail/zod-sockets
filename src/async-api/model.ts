2284b75f3ff28ffc16924139051c31ce661b0403
Robin Tail <robin_tail@me.com>
2024-04-01 19:48:17 +0200

    Ref: moving to a separate file.

--- src/async-api/model.ts
+++ src/async-api/model.ts
@@ -1,3 +1,4 @@
+import { SecuritySchemeObject } from "./security";
 import { WS } from "./websockets";
 
 /**
@@ -322,58 +323,6 @@ export interface TagObject {
   name: string;
   description?: string;
   externalDocs?: ExternalDocumentationObject;
-}
-
-export interface SecuritySchemeObject {
-  type:
-    | "userPassword"
-    | "apiKey"
-    | "X509"
-    | "symmetricEncryption"
-    | "asymmetricEncryption"
-    | "httpApiKey"
-    | "http"
-    | "oauth2"
-    | "openIdConnect"
-    | "plain"
-    | "scramSha256"
-    | "scramSha512"
-    | "gssapi";
-  description?: string;
-  /** @desc for httpApiKey */
-  name?: string;
-  /** @desc Valid values are "user" and "password" for "apiKey" and "query", "header" or "cookie" for "httpApiKey". */
-  in?: "user" | "password" | "query" | "header" | "cookie";
-  /**
-   * @desc for http
-   * @link https://www.iana.org/assignments/http-authschemes/http-authschemes.xhtml
-   * */
-  scheme?: string;
-  /** @desc for http */
-  bearerFormat?: string;
-  /** @desc for oauth2 */
-  flows?: OAuthFlowsObject;
-  /** @desc for openIdConnect */
-  openIdConnectUrl?: string;
-  /** @desc List of the needed scope names. For "oauth2" and "openIdConnect" */
-  scopes?: string[];
-}
-
-export interface OAuthFlowsObject {
-  implicit?: OAuthFlowObject;
-  password?: OAuthFlowObject;
-  clientCredentials?: OAuthFlowObject;
-  authorizationCode?: OAuthFlowObject;
-}
-
-export interface OAuthFlowObject {
-  /** @desc for implicit and authorizationCode */
-  authorizationUrl?: string;
-  /** @desc for password, clientCredentials and authorizationCode */
-  tokenUrl?: string;
-  refreshUrl?: string;
-  /** @desc A map between the scope name and a short description for it. */
-  availableScopes: Record<string, string>;
 }
 
 /** @since 3.0.0 partially extends SchemaObject; schema prop removed */

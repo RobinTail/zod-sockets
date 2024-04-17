911dba9766d08caccdd06750d507627a8811336f
Anna Bocharova <robin_tail@me.com>
2024-04-17 21:42:07 +0200

    Moving logger from config into `attachSockets()` (#134)
    * Moving logger from config into attachSockets().
    
    * Readme: adjusting the logger customization advices.
    
    * Changelog: 0.20.0.
    
    * Minor: todo.

--- src/config.spec.ts
+++ src/config.spec.ts
@@ -1,14 +1,12 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import { z } from "zod";
 import { Config, createSimpleConfig } from "./config";
-import { AbstractLogger } from "./logger";
 
 describe("Config", () => {
   describe("::constructor", () => {
     test("should create config without any argument", () => {
       const config = new Config();
       expect(config).toBeInstanceOf(Config);
-      expect(config.logger).toEqual(console);
       expect(config.timeout).toBe(2000);
       expect(config.namespaces).toEqual({});
     });
@@ -30,10 +28,8 @@ describe("Config", () => {
           },
         },
         timeout: 3000,
-        logger: { debug: vi.fn() } as unknown as AbstractLogger,
       });
       expect(config).toBeInstanceOf(Config);
-      expect(config.logger).toEqual({ debug: expect.any(Function) });
       expect(config.timeout).toBe(3000);
       expect(config.namespaces).toEqual({
         "/": {
@@ -56,7 +52,6 @@ describe("Config", () => {
     test("should provide namespace augmentation method", () => {
       const base = new Config();
       expect(base).toBeInstanceOf(Config);
-      expect(base.logger).toEqual(console);
       expect(base.timeout).toBe(2000);
       expect(base.namespaces).toEqual({});
       const schema = z.tuple([]);
@@ -80,7 +75,6 @@ describe("Config", () => {
     test("should set defaults", () => {
       const config = createSimpleConfig();
       expect(config).toBeInstanceOf(Config);
-      expect(config.logger).toEqual(console);
       expect(config.timeout).toBe(2000);
       expect(config.namespaces).toEqual({
         "/": {
@@ -95,13 +89,11 @@ describe("Config", () => {
     test("should set the root namespace properties", () => {
       const config = createSimpleConfig({
         timeout: 3000,
-        logger: { debug: vi.fn() } as unknown as AbstractLogger,
         emission: {},
         hooks: {},
         metadata: z.object({}),
       });
       expect(config).toBeInstanceOf(Config);
-      expect(config.logger).not.toEqual(console);
       expect(config.timeout).toBe(3000);
       expect(config.namespaces).toEqual({
         "/": {

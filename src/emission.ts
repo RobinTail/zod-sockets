99d2411190a94ce6274711a6a786571c640070e9
Robin Tail <robin_tail@me.com>
2024-08-04 21:26:01 +0200

    REF: introducing parseWrapped().

--- src/emission.ts
+++ src/emission.ts
@@ -1,7 +1,11 @@
 import assert from "node:assert/strict";
 import type { RemoteSocket, Server, Socket } from "socket.io";
 import { z } from "zod";
-import { InputValidationError, OutputValidationError } from "./errors";
+import {
+  InputValidationError,
+  OutputValidationError,
+  parseWrapped,
+} from "./errors";
 import {
   RemoteClient,
   SomeRemoteSocket,
@@ -72,27 +76,6 @@ export function makeEmitter({
     const { schema, ack } = emission[event];
     return { schema, ack: ack && ("id" in subject ? ack : ack.array()) };
   };
-  const parseOutput = (schema: z.AnyZodTuple, payload: unknown[]) => {
-    try {
-      return schema.parse(payload);
-    } catch (error) {
-      throw error instanceof z.ZodError
-        ? new OutputValidationError(error)
-        : error;
-    }
-  };
-  const parseAck = (
-    schema: z.AnyZodTuple | z.ZodArray<z.AnyZodTuple>,
-    payload: unknown[],
-  ) => {
-    try {
-      return schema.parse(payload);
-    } catch (error) {
-      throw error instanceof z.ZodError
-        ? new InputValidationError(error)
-        : error;
-    }
-  };
   /**
    * @throws OutputValidationError on validating emission
    * @throws InputValidationError on validating acknowledgment
@@ -101,14 +84,14 @@ export function makeEmitter({
   return async (event: string, ...args: unknown[]) => {
     assert(event in emission, new Error(`Unsupported event ${event}`));
     const { schema, ack } = getSchemas(event);
-    const payload = parseOutput(schema, args);
+    const payload = parseWrapped(schema, args, OutputValidationError);
     if (!ack) {
       return subject.emit(String(event), ...payload) || true;
     }
     const response = await subject
       .timeout(timeout)
       .emitWithAck(String(event), ...payload);
-    return parseAck(ack, response);
+    return parseWrapped(ack, response, InputValidationError);
   };
 }
 

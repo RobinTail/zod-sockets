dabd3ebb096fd1965916d0c8c46a90c53b4ee6b9
Robin Tail <robin_tail@me.com>
2026-03-15 15:42:16 +0100

    fix(test): ensureError test.

--- zod-sockets/src/common-helpers.spec.ts
+++ zod-sockets/src/common-helpers.spec.ts
@@ -67,7 +67,7 @@ describe("Common helpers", () => {
       [Symbol("symbol"), "Symbol(symbol)"],
       [true, "true"],
       [false, "false"],
-      [() => {}, "() => {\n      }"],
+      [() => {}, "() => {}"],
       [/regexp/is, "/regexp/is"],
       [[1, 2, 3], "1,2,3"],
     ])("should accept %#", (argument, expected) => {

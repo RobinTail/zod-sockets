bb0515f66b66a42b81102603f3e4f4967eb19dbd
Robin Tail <robin_tail@me.com>
2026-03-15 18:55:18 +0100

    fix: ensure givePort for example itself.

--- example/index.ts
+++ example/index.ts
@@ -3,10 +3,11 @@ import { attachSockets } from "zod-sockets";
 import { actions } from "./actions";
 import { config } from "./config";
 import { Server } from "socket.io";
+import { givePort } from "../tools/ports";
 
 attachSockets({
   io: new Server(),
-  target: http.createServer().listen(8090),
+  target: http.createServer().listen(givePort("example")),
   config,
   actions,
 });

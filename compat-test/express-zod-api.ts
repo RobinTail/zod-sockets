88c60b7e53675cad23a99177c6ea06021f9a7941
Robin Tail <robin_tail@me.com>
2026-05-12 08:17:42 +0200

    fix(CI): add integration testing to compatibility test.

--- compat-test/express-zod-api.ts
+++ compat-test/express-zod-api.ts
@@ -1,6 +1,6 @@
 import { createConfig, createServer } from "express-zod-api";
 import { Server } from "socket.io";
-import { attachSockets, createSimpleConfig } from "zod-sockets";
+import { attachSockets, createSimpleConfig, Integration } from "zod-sockets";
 import { givePort } from "../tools/ports";
 
 const serverConfig = createConfig({
@@ -11,6 +11,16 @@ const serverConfig = createConfig({
 const { servers, logger } = await createServer(serverConfig, {});
 
 const socketsConfig = createSimpleConfig();
+
+logger.info(
+  "Integration sample",
+  (
+    await Integration.create({
+      config: socketsConfig,
+      actions: [],
+    })
+  ).print(),
+);
 
 const io = new Server();
 await attachSockets({

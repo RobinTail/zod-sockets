3346d800b93eab55976cb25d70da8086bfdf414c
Robin Tail <robin_tail@me.com>
2026-07-18 12:28:50 +0200

    fix the patch.

--- .pnpmfile.mjs
+++ .pnpmfile.mjs
@@ -1,14 +1,11 @@
 /**
  * Babel v8 changed their engines compatibility in rc5 from 24.0.0 to 24.11.0 with no good reason
+ * Babel usage was removed from tsdown 0.22.4 tree, but its engine restrictions remained
  * @todo remove this hack in next major
  */
 function readPackage(pkg, ctx) {
-  // Target the specific breaking version of babel types
-  if (
-    pkg.name &&
-    pkg.name.startsWith("@babel/") &&
-    pkg.version.startsWith("8.0.0")
-  ) {
+  if (!pkg.name) return pkg;
+  if (["rolldown-plugin-dts", "tsdown"].includes(pkg.name)) {
     pkg.engines = {
       ...pkg.engines,
       node: "^22.18.0 || >=24.0.0", // Force compatibility
